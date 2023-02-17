c6ddeeea584debad9bc4d637a9fd9e064ac51ed8
unknown <mahekmj123@gmail.com>
2023-02-18 05:24:09 +0530

    chatbot added

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
       {/* <Home /> */}
-      <Chat/>
+      {/* <Chat/> */}
       <App />
     </ContextProvider>
   </React.StrictMode>,
