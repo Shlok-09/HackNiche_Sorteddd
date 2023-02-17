bd7e87e7a1a2bf220a0f7a36c7ebb3a9a9e85649
Rajoshi11 <ruju111202@gmail.com>
2023-02-17 17:58:22 +0530

    Home Landing Page

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
+import Home from './Home';
 
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
-      <App />
+      <Home />
+      {/* <App /> */}
     </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
