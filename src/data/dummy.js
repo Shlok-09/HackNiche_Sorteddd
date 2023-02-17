7ffef95e516b900c60f9d35ad3a43f4fd6e882ff
Rajoshi11 <ruju111202@gmail.com>
2023-02-17 13:30:08 +0530

    dashboard page

---
+++ src/data/dummy.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { AiOutlineCalendar, AiOutlineShoppingCart, AiOutlineAreaChart, AiOutlineBarChart, AiOutlineStock } from 'react-icons/ai';
+import { FiShoppingBag, FiEdit, FiPieChart, FiBarChart, FiCreditCard, FiStar, FiShoppingCart } from 'react-icons/fi';
+import { BsKanban, BsBarChart, BsBoxSeam, BsCurrencyDollar, BsShield, BsChatLeft } from 'react-icons/bs';
+import { BiColorFill } from 'react-icons/bi';
+import { IoMdContacts } from 'react-icons/io';
+import { RiContactsLine, RiStockLine } from 'react-icons/ri';
+import { MdOutlineSupervisorAccount } from 'react-icons/md';
+import { HiOutlineRefresh } from 'react-icons/hi';
+import { TiTick } from 'react-icons/ti';
+import { GiLouvrePyramid } from 'react-icons/gi';
+import { GrLocation } from 'react-icons/gr';
+
+export const themeColors = [
+    {
+      name: 'blue-theme',
+      color: '#1A97F5',
+    },
+    {
+      name: 'green-theme',
+      color: '#03C9D7',
+    },
+    {
+      name: 'purple-theme',
+      color: '#7352FF',
+    },
+    {
+      name: 'red-theme',
+      color: '#FF5C8E',
+    },
+    {
+      name: 'indigo-theme',
+      color: '#1E4DB7',
+    },
+    {
+      color: '#FB9678',
+      name: 'orange-theme',
+    },
+  ];
+  
+
+export const links = [
+    {
+      title: 'Dashboard',
+      links: [
+        {
+          name: 'ecommerce',
+          icon: <FiShoppingBag />,
+        },
+      ],
+    },
+    {
+      title: 'Apps',
+      links: [
+        {
+          name: 'calendar',
+          icon: <AiOutlineCalendar />,
+        },
+        {
+          name: 'kanban',
+          icon: <BsKanban />,
+        },
+      ],
+    },
+    {
+      title: 'Charts',
+      links: [
+        {
+          name: 'line',
+          icon: <AiOutlineStock />,
+        },
+        {
+          name: 'area',
+          icon: <AiOutlineAreaChart />,
+        },
+  
+        {
+          name: 'bar',
+          icon: <AiOutlineBarChart />,
+        },
+        {
+          name: 'pie',
+          icon: <FiPieChart />,
+        },
+        {
+          name: 'financial',
+          icon: <RiStockLine />,
+        },
+        {
+          name: 'color-mapping',
+          icon: <BsBarChart />,
+        },
+        {
+          name: 'pyramid',
+          icon: <GiLouvrePyramid />,
+        },
+        {
+          name: 'stacked',
+          icon: <AiOutlineBarChart />,
+        },
+      ],
+    },
+  ];
